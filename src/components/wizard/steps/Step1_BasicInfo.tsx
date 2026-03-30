22456194e4752e2fb98e5b19c7d266223a40d12c
netelip <netelip@centrodemando.es>
2026-03-30 18:22:56 +0200

    Fix: Removed unused useState import in Step1_BasicInfo to resolve Vercel build error

--- src/components/wizard/steps/Step1_BasicInfo.tsx
+++ src/components/wizard/steps/Step1_BasicInfo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useWizardStore } from '../../../store/wizardStore';
 import { WizardStepHeader } from '../WizardStepHeader';
 
